087c66382dff37aca9b6190d8ef00a5a3881f74e
“Anne6x24” <Anne6x24@stud.kea.dk>
2022-05-27 12:09:39 +0200

    anne myyyy

---
+++ script/hero.js
@@ -0,0 +1,14 @@
+var text = ['bæredygtige', 'hæklet i københavn', 'håndlavede'];
+
+var index = 0;
+
+function changeText() {
+  document.getElementById("forside_hero_text").innerText = text[index];
+  index = (index + 1) % 3;
+}
+
+var interval = window.setInterval(changeText, 2000);
+
+window.onunload = function () {
+  window.clearInterval(interval);
+}

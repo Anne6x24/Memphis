a6ed33ae4e38205b675131de96118a7810779151
Katja Levring <katj6233@stud.kea.dk>
2022-05-30 22:45:21 +0200

    modal til forside

--- script/popup.js
+++ script/popup.js
@@ -2,10 +2,6 @@ const modalReview = document.querySelector("#modal-singleview");
 const openModalReview = document.querySelector(".open-button-singleview");
 const closeModalReview = document.querySelector(".close-button-singleview");
 
-const modalEmail = document.querySelector("#modal-email");
-const openModalEmail = document.querySelector(".open-button-email");
-const closeModalEmail = document.querySelector(".close-button-email");
-
 // singleview modal
 openModalReview.addEventListener("click", () => {
   modalReview.showModal();
@@ -13,13 +9,4 @@ openModalReview.addEventListener("click", () => {
 
 closeModalReview.addEventListener("click", () => {
   modalReview.close();
-});
-
-// email modal
-openModalEmail.addEventListener("click", () => {
-  modalEmail.showModal();
-});
-
-closeModalEmail.addEventListener("click", () => {
-  modalEmail.close();
 });
---
+++ script/popupemail.js
@@ -0,0 +1,12 @@
+const modalEmail = document.querySelector("#modal-email");
+const openModalEmail = document.querySelector(".open-button-email");
+const closeModalEmail = document.querySelector(".close-button-email");
+
+// email modal
+// openModalEmail.addEventListener("click", () => {
+//   modalEmail.showModal();
+// });
+
+closeModalEmail.addEventListener("click", () => {
+  modalEmail.close();
+});
